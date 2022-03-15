b5cbdfadda11692e012c07fd220ddb9cd90b8249
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-03-15 13:17:21 +0000

    import of the file

--- coursework.py
+++ coursework.py
@@ -1,1 +1,11 @@
-print("Team 6 members are:Luke,...") 
+import csv
+
+filename = 'data.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+    
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)#
+
