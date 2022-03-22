fb8d6eb81d9214e855c525ba7481f8980cda1dde
ShenalMendis <mendisshenal@gmail.com>
2022-03-22 13:30:41 +0000

    something

--- coursework.py
+++ coursework.py
@@ -1,12 +1,36 @@
-
 import csv
+from datetime import date, datetime
 import matplotlib.pyplot as plt
 
-filename = 'data.csv'
+
+filename = 'Coursework-4216COMP-1/data.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
     
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)#
+    def LAT():
+        date,LAT_Uncertainty = [],[]
+        for row in reader:
+    
+            temp_date = datetime.strptime(row[1],'%d/%m/%Y')
+            date.append(temp_date)
+
+            temp_LAT_Uncertainty = float(row[3])
+            LAT_Uncertainty.append(temp_LAT_Uncertainty)
+            #print(row[1]+"\t"+row[3])
+
+    
+        plt.style.use('seaborn')
+        fig, ax = plt.subplots()
+        ax.plot(date,LAT_Uncertainty, c='red')
+
+
+        ax.set_title("Graph", fontsize=24)
+        ax.set_xlabel('Land Average Temperature Uncertainty', fontsize=10)
+        fig.autofmt_xdate()
+        ax.set_ylabel("Temperature (F)", fontsize=10)
+        ax.tick_params(axis='both', which='major', labelsize=16)
+        
+
+        plt.show()
+    LAT()
--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.scatter(2, 4, s=200)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+ax.tick_params(axis='both', which='major', labelsize=14)
 
 plt.show()
