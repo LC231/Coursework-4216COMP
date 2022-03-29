84c7497711a11ab2e487d8dd4e3a5b3062d3141a
231lukecurran@gmail.com <231lukecurran@gmail.com>
2022-03-29 13:30:57 +0100

    My function

--- coursework.py
+++ coursework.py
@@ -7,6 +7,9 @@ with open('M:/4216COMP coursework/Coursework-4216COMP/data.csv') as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+    
     def max_Temp():
         max_temps, dates = [],[]
         
@@ -22,16 +25,35 @@ with open('M:/4216COMP coursework/Coursework-4216COMP/data.csv') as f:
         
         plt.style.use('seaborn')
         fig, ax = plt.subplots()
-        ax.plot(dates, max_temps, c='red')
+        ax.plot(dates, max_temps,'ok-')
 
   
-        ax.set_title("monthly max temperatures", fontsize=24)
+        ax.set_title("Max Temperatures", fontsize=24)
         ax.set_xlabel('Dates', fontsize=16)
         fig.autofmt_xdate()
-        ax.set_ylabel("Temperature (F)", fontsize=16)
+        ax.set_ylabel("Temperature (C)", fontsize=16)
         ax.tick_params(axis='both', which='major', labelsize=16)
         ax.set_yticks(range(1,26,2))
         ax.set_ylim(1,26)
         
         plt.show()  
     max_Temp()      
+
+print("1 = Land Average Temperature")
+print("2 = Land Average Tmeprature Uncertainty")
+print("3 = Land Max Temperature")
+print("4 = Land Max Temperature Uncertainty")
+print("5 = Land Min Temperature")
+print("6 = Land Min Temperature uncertainty")
+print("7 = Land and Ocean Average Temperature")
+print("8 = Land and Ocean Average Temperature Uncertainty")
+# choice = input("Please enter a number between 1-8:")
+
+# if choice == '1':
+#     elif choice =='2':
+#         elif choice =='3':
+#             elif choice =='4':
+#                 elif choice =='5':
+#                     elif choice =='6':
+#                         elif choice =='7':
+#                             elif choice =='8':
