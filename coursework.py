0940c548b0a93201f9971da0ce95638ca176fe0e
Luke <97961686+LukeCitrine@users.noreply.github.com>
2022-03-31 15:34:38 +0100

    courseworkvis1
    Land and Ocean average temp uncertainty

--- coursework.py
+++ coursework.py
@@ -1,31 +1,39 @@
-
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
 
-with open('M:/4216COMP coursework/Coursework-4216COMP/data.csv') as f:
+with open('data.csv') as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
     def max_Temp():
-        max_temps, dates = [],[]
+        max_temps, min_temps, dates = [],[],[]
+        
         for row in reader:
-            max_temp = float(row[4])
+            max_temp = float(row[8])
+            min_temp = float(row[9])
+
+            min_temps.append(min_temp)
             max_temps.append(max_temp)
             date = datetime.strptime(row[1],'%d/%m/%Y')
             dates.append(date)
+            
+        dates=dates[::10]
+        max_temps = max_temps[::10]
+        min_temps = min_temps[::10]
 
-         
-        plt.style.use('seaborn')
+#Create extra plots
+
+#New test
         fig, ax = plt.subplots()
-        ax.plot(dates, max_temps, c='red')
+        ax.plot(dates, min_temps, c='blue')
 
-  
-        ax.set_title("monthly max temperatures", fontsize=24)
-        ax.set_xlabel('', fontsize=16)
+        ax.set_title("Land and Ocean average temperature uncertainty", fontsize=15)
+        ax.set_xlabel('Dates', fontsize=16)
         fig.autofmt_xdate()
         ax.set_ylabel("Temperature (F)", fontsize=16)
-        ax.tick_params(axis='both', which='major', labelsize=16)
 
-        plt.show()  
-    max_Temp()      
+        plt.show()
+    
+    max_Temp()   
