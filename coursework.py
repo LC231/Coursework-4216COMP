707660fcbee8e42acdc7760b7f42212f8a8d4204
AmbitiousOne <97476939+AmbitiousOne@users.noreply.github.com>
2022-04-05 15:56:42 +0100

    Update errors
    I have improved the titles and corrected the row value.

--- coursework.py
+++ coursework.py
@@ -1,4 +1,3 @@
-
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -7,25 +6,27 @@ with open('M:/4216COMP coursework/Coursework-4216COMP/data.csv') as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    def max_Temp():
-        max_temps, dates = [],[]
+    def min_Temp():
+        min_temps, dates = [],[]
         for row in reader:
-            max_temp = float(row[4])
-            max_temps.append(max_temp)
+            min_temp = float(row[9])
+            min_temps.append(min_temp)
             date = datetime.strptime(row[1],'%d/%m/%Y')
             dates.append(date)
 
+        dates=dates[::10]
+        min_temps = min_temps[::10]
          
         plt.style.use('seaborn')
         fig, ax = plt.subplots()
-        ax.plot(dates, max_temps, c='red')
+        ax.plot(dates, min_temps, c='blue')
 
   
-        ax.set_title("monthly max temperatures", fontsize=24)
+        ax.set_title("monthly min temperatures", fontsize=24)
         ax.set_xlabel('', fontsize=16)
         fig.autofmt_xdate()
         ax.set_ylabel("Temperature (F)", fontsize=16)
         ax.tick_params(axis='both', which='major', labelsize=16)
 
         plt.show()  
-    max_Temp()      
+    min_Temp()      
